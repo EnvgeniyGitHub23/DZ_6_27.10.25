df940ba264c7d3fcca973f795d7d2a7f79262823
Теппонен Евгений <etepponen@metaprime.ru>
2025-10-29 11:32:44 +0300

    ДЗ №6

---
+++ app/src/main/java/ru/evgenious/college/MainActivity.java
@@ -0,0 +1,33 @@
+package ru.evgenious.college;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+
+/*
+1 Создать проект в Андроид студии со следующими пользовательскими элементами:
+ TextView, EditText, Button, Switch, CheckBox.
+  Использовать LinearLayout+ConstraintLayout
+
+2* (по желанию) добавить CalendarView
+*/
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
